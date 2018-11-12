71138a592a9484dfd138fdf54882ed791e730c56
shants <mailtoshants@gmail.com>
2018-11-12 18:33:53 -0500

    53 good problem

--- 53.py
+++ 53.py
@@ -4,14 +4,21 @@ class Solution(object):
 		:type nums: List[int]
 		:rtype: int
 		"""
+
+		if len(nums) == 0:
+			return 0
+		if len(nums) == 1:
+			return nums[0]
+
 		maxEndHere = nums[0]
 		maxSoFar = nums[0]
-		for i in range(len(nums)):
-			maxEndHere = max(nums[i], nums[i]+maxEndHere)
-			maxSoFar = max(maxEndHere,maxSoFar)
+		for i in range(1,len(nums)):
+			maxEndHere = max(nums[i], nums[i] + maxEndHere)
+			maxSoFar = max(maxEndHere, maxSoFar)
 		return maxSoFar
+
 
 if __name__ == '__main__':
 	s = Solution()
-	print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+	print(s.maxSubArray([1,2]))
 
