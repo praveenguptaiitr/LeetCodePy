274fca90c3ec4f04ec4ab9c0227559d7b916474f
shants <mailtoshants@gmail.com>
2018-11-15 22:41:12 -0500

    557

--- 557.py
+++ 557.py
@@ -5,6 +5,25 @@ class Solution:
 		:rtype: str
 		"""
 
-		s1 = s.split()
-		s = " ".join([i[::-1] for i in s1])
+		# s1 = s.split()
+		# s = " ".join([i[::-1] for i in s1])
+		# return s
+
+		i=0
+		ss = 0
+		se = 0
+		while(i<len(s)):
+			while(i < len(s) and s[i]==" "):
+				i+=1
+			ss = i
+			while(i < len(s) and s[i]!= " "):
+				i+=1
+			se = i
+			s1 = s[0:ss]+ s[ss:se][::-1]+s[se:]
+			s = s1
+
 		return s
+
+if __name__ == "__main__":
+	s = Solution()
+	print(s.reverseWords("abc     efg   hij"))
