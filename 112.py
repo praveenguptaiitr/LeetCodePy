d725accd78cdb87bdaa35dfe3325634e6266631f
shants <mailtoshants@gmail.com>
2018-11-29 12:31:48 -0500

    112

---
+++ 112.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+
+        if root is None and sum == 0:
+            return False
+        if root is None and sum != 0:
+            return False
+
+        if root.left is None and root.right is None and sum - root.val == 0:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or (self.hasPathSum(root.right, sum - root.val))
