e1abe017b48b53217ba00c2a8cca16b6bc0abb7b
shants <mailtoshants@gmail.com>
2018-11-30 20:36:09 -0500

    5 IMP : sometimes top down doesnt work , you need to do bottom up to avoid TLE

--- 5.py
+++ 5.py
@@ -1,37 +1,48 @@
 class Solution:
 
-    def findLongest(self,str, tbl,i,j):
-        if i > j:
-            return -1
-
-        if tbl[i][j]!=-1:
-            return tbl[i][j]
-
-        if str[i]==str[j]:
-            tbl[i][j]= 2+self.findLongest(str,tbl,i+1,j-1)
-        else:
-            l1,l2 = -1,-1
-            l1=self.findLongest(str,tbl,i+1,j)
-            l2= self.findLongest(str,tbl,i,j-1)
-            tbl[i][j]=max(l1,l2)
-
-        return tbl[i][j]
-
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if s is None :
-            return 0
+        if s is None or len(s)==0:
+            return ""
+        if len(s)==1:
+            return s[0]
+        if len(s)==2:
+            if s[0]==s[1]:
+                return s
+            else:
+                return s[0]
 
-        tbl = [ [-1]*(len(s)) for i in range(len(s))]
-        for i in range(len(s)):
-            tbl[i][i]=1
-        p = self.findLongest(s,tbl,0,len(s)-1)
-        return tbl
+        n = len(s)
+        start = 0
+        end = 0
+        sz = 1
+        tbl = [[False]* len(s) for r in range(n)]
+        for i in range(n):
+            tbl[i][i]=True
+
+        for i in range(n-1):
+            if s[i]==s[i+1]:
+                tbl[i][i+1]=True
+                start = i
+                end = i+1
+                sz = 2
+
+        for k in range(3, n+1):
+            for i in range(0, n-k+1):
+                j = i+k-1
+                if tbl[i+1][j-1] and s[i]==s[j]:
+                    tbl[i][j]=True
+                    if k > sz:
+                        start = i
+                        end =j
+                        sz = k
+
+        return s[start:end+1]
 
 if __name__=="__main__":
     s = Solution()
-    k = s.longestPalindrome("babad")
+    k = s.longestPalindrome("abbc")
     print(k)
