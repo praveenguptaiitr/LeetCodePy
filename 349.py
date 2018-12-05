0bbd923dddccc696f5f0ea77d1125653799f10ae
shants <mailtoshants@gmail.com>
2018-12-04 20:15:58 -0500

    349

---
+++ 349.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        ans = {}
+        for n in nums1:
+            if n in nums2:
+                ans[n]=1
+
+        return ans.keys()
