403965dcab13cd926269eef3e204ce1b51602a3b
shants <mailtoshants@gmail.com>
2019-01-13 18:31:30 -0500

    692

--- 692.py
+++ 692.py
@@ -1,12 +1,21 @@
-#from functools import  cmp_to_key
+from functools import  cmp_to_key
 import heapq
-# def compare_key_new(a, b):
-#     if a[0] < b[0]:
-#         return -1
-#     elif a[0]>b[0]:
-#         return 1
-#     else:
-#         return a[1]>b[1]
+def compare_key_new(a, b):
+    if a[0]==b[0]:
+        if a[1]==b[1]:
+            return 0
+        if a[1]<b[1]:
+            return -1
+        else:
+            return 1
+    else:
+        if a[0]==b[0]:
+            return 0
+        if a[0]>b[0]:
+            return -1
+        else:
+            return 1
+
 
 class Solution(object):
     def topKFrequent(self, words, k):
@@ -20,16 +29,19 @@ class Solution(object):
             d[w]=d.get(w,0)+1
         l = []
         for k1,v1 in d.items():
-            l.append((-v1,k1))
+            l.append((v1,k1))
 
-        heapq.heapify(l)
+        cmp = cmp_to_key(compare_key_new)
+
+        l.sort(key=cmp)
         ans = []
         for i in range(k):
-            ans.append(heapq.heappop(l)[1])
+            ans.append(l[i][1])
+
         return ans
 
 if __name__ == "__main__":
     s = Solution()
     print(s.topKFrequent(["aaa","aa","a"], 1))
     print(s.topKFrequent(["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],4))
-    print(s.topKFrequent(["i", "love", "leetcode", "i", "love", "coding"],2))
+    #print(s.topKFrequent(["i", "love", "leetcode", "i", "love", "coding"],2))
