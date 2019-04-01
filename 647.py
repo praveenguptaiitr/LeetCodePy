347a59078acc876210d8840f361fa1f7607ed61f
shants <mailtoshants@gmail.com>
2019-03-31 22:33:11 -0400

    647 : new understandable code

--- 647.py
+++ 647.py
@@ -17,11 +17,23 @@ class Solution(object):
         count = 0
         n = len(s)-1
         dp = [[False]*len(s) for a in range(len(s))]
-        for d in range(len(s)):
-            for i in range(len(s)-d):
-                j = i+d
+        for i in range(n+1):
+            dp[i][i]=True
+            count+=1
+
+        for i in range(n):
+            if s[i]==s[i+1]:
+                dp[i][i+1]=True
+                count+=1
+
+        for k in range(2,n+1):
+            for i in range(n-k+1):
+                j = i+k
+                if i==j:
+                    dp[i][j]=True
                 if s[i]==s[j]:
-                    dp[i][j]= True if i+1>= j-1 else dp[i+1][j-1]
+                    if i+1<n:
+                        dp[i][j]= dp[i+1][j-1]
                 if s[i]!= s[j]:
                     dp[i][j]=False
                 if dp[i][j]==True:
@@ -32,4 +44,6 @@ class Solution(object):
 
 if __name__ == "__main__":
     s = Solution()
+    print(s.countSubstrings("aba"))
+    print(s.countSubstrings("abc"))
     print(s.countSubstrings("aaa"))
